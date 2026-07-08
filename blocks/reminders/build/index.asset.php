b2362a24e48102e86c1eacd684ce1c557cf78575
Tsjippy <ewaldha@hotmail.com>
2026-07-08 15:21:10 +0200

    changed: removed vaccination reminders block

--- blocks/reminders/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5efcf0e5d8b616daf5d1');
