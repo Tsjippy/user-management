810d105b822c855722850ea37103fb8b6a1536f9
Tsjippy <ewaldha@hotmail.com>
2026-07-12 16:36:25 +0200

    fixed: activation

--- tsjippy-user-management.php
+++ tsjippy-user-management.php
@@ -108,3 +108,39 @@ register_deactivation_hook(__FILE__, function () {
             wp_delete_post(SETTINGS[$page], true);
     }
 });
+
+/**
+ * Creates default pages if needed
+ */
+function createDefaultPages($returnKey=''){
+    /**
+     *  Default pages
+     */
+    $settings    = SETTINGS;
+
+    // Create account page
+    if(!isset($settings['account_page'])){
+        $settings['account_page']           = TSJIPPY\ADMIN\createDefaultPage('Account', 'tsjippy_user-info currentuser=true]');
+    }
+
+    if(!isset($settings['user-edit-page'])){
+        // Create user edit page
+        $settings['user-edit-page']         = TSJIPPY\ADMIN\createDefaultPage('Edit users', '[tsjippy_user-info]');
+    }
+
+    if(!isset($settings['account-create-page'])){
+        // Create user create page
+        $settings['account-create-page']    = TSJIPPY\ADMIN\createDefaultPage('Add user account', '[tsjippy_create_user_account]');
+    }
+
+    if(!isset($settings['pending-users-page'])){
+        // Create pending users page
+        $settings['pending-users-page']     = TSJIPPY\ADMIN\createDefaultPage('Pending user accounts', '<!-- wp:tsjippy-user-management/pending-user-accounts /-->');
+    }
+
+    update_option('tsjippy_' . PLUGINSLUG . '_settings', $settings);
+
+    if(!empty($returnKey) && isset($settings[$returnKey])){
+        return $settings[$returnKey];
+    }
+}
