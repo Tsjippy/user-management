873f381cc1653a1ae934e9ce47b558b47e80e43f
Tsjippy <ewaldha@hotmail.com>
2026-09-12 07:25:23 +0200

    changed: removed 'tested up to'

--- tsjippy-user-management.php
+++ tsjippy-user-management.php
@@ -12,11 +12,10 @@ use TSJIPPY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Tested up to:         7.1
- * Plugin URI:            https://github.com/Tsjippy/usermanagement
+ * Plugin URI:           https://github.com/Tsjippy/usermanagement
  * Tested:               7.1
- * TextDomain:            tsjippy
- * Requires Plugins:    , tsjippy-forms
+ * TextDomain:           tsjippy
+ * Requires Plugins:     tsjippy-forms
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
